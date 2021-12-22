93e2d53e340b0d57f7666a0cba7fad5ee72d955a
Diana Deluvian <diana.deluvian@gmail.com>
2021-12-22 14:31:47 +0100

    set up basic routes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,3 +1,29 @@
-export default function App() {
-  return <h1 className='text-3xl font-bold underline'> Hello world! </h1>;
-}
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className=''>
+        <Routes>
+          <Route exact path='/' element={<Posts />} />
+          <Route exact path='/post/:_id' element={<SinglePost />} />
+          <Route exact path='/addPost' element={<AddPost />} />
+          <Route exact path='/editPost/:_id' element={<EditPost />} />
+
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/login' element={<Login />} />
+
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
