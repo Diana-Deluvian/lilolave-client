2a423aae86ba452c11062231945b267cc1b8fc15
Diana Deluvian <diana.deluvian@gmail.com>
2021-12-23 09:27:33 +0100

    added search slice

---
+++ src/features/search/searchSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const categories = [
+  'Essays',
+  'Poems',
+  'Reviews',
+  'Game Analysis',
+  'Series Study',
+  'Film Critique',
+];
+
+export const searchSlice = createSlice({
+  name: 'search',
+  initialState: { keyword: '', category: '' },
+  reducers: {
+    setSearchKeyword: (state, action) => {
+      state.keyword = action.payload;
+    },
+    clearSearchKeyword: (state) => {
+      state.keyword = '';
+    },
+    setSearchCategory: (state, action) => {
+      state.category = action.payload;
+    },
+    clearSearchCategory: (state) => {
+      state.category = '';
+    },
+  },
+});
+
+export const {
+  setSearchKeyword,
+  clearSearchKeyword,
+  setSearchCategory,
+  clearSearchCategory,
+} = searchSlice.actions;
+
+export const selectSearchKeyword = (state) => state.search.keyword;
+export const selectSearchCategory = (state) => state.search.category;
+
+export default searchSlice.reducer;
