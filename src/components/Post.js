c500c2cd95b5799fa7000a1c55e79358728c67ed
Diana Deluvian <diana.deluvian@gmail.com>
2021-12-28 10:55:43 +0100

    added category to thumbpost

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,8 +5,12 @@ export default function Post({ post }) {
   return (
     <div className='flex flex-col w-full border-l-2 border-t-2 border-gray-200  bg-gray-100 postShadow justify-center px-2 max-width-80ch ml-4 mr-8 lg:px-4 lg:mx-0 my-6'>
       <Link to={`/post/${post._id}`}>
-        <span className='text-xl'> {post?.title || 'No title provided'} </span>
-
+        <div className='flex justify-between'>
+          <span className='text-xl'>{post?.title || 'No title provided'}</span>
+          <span className='text-gray-600 mr-2 self-center'>
+            {post?.category}
+          </span>
+        </div>
         <p className='text-gray-600 my-2 italic'>{post?.date}</p>
         <div className='my-3'>
           {post?.keywords?.split(', ').map((keyword) => (
@@ -19,8 +23,8 @@ export default function Post({ post }) {
           ))}
         </div>
         <p className='mb-4'>
-          {`${post?.content?.split(/\r?\n/)[0].substring(0, 100)} `}{' '}
-          <span className='text-gray-600'>(...read more)</span>
+          {`${post?.content?.split(/\r?\n/)[0].substring(0, 100)}- `}{' '}
+          <span className='text-gray-600 text-sm '>(...read more)</span>
         </p>
         {
           //the above returns at most the first 100 chars of the first paragraph of a post
